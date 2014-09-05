e3f8e8353b72f0eb686120a7d355799701773656
Chris Czub <chris.czub@gmail.com>
2014-09-04 21:07:22 -0400

    code getting refactored somewhat, working on challenge 6

--- set1.py
+++ set1.py
@@ -8,6 +8,9 @@ import string
 import math
 
 
+ASSERT = True
+
+
 def hex2bin(hexStr):
     return bytearray(binascii.unhexlify(hexStr))
 
@@ -20,6 +23,11 @@ assert b64(hex2bin("49276d206b696c6c696e6720796f757220627261696e206c696b65206120
 
 
 def repeating_xor(buf, key):
+    message = bytearray()
+    if not type(buf) == type(message) == type(key):
+        print type(buf), type(key)
+    assert type(buf) == type(message) and type(key) == type(message)
+
     _l = len(buf)
 
     # pad key out to full length
@@ -28,11 +36,11 @@ def repeating_xor(buf, key):
         keyPad = keyPad[:_l]
     assert len(keyPad) == _l
 
-    message = bytearray()
     for i, c in enumerate(buf):
         message.append(c ^ keyPad[i])
 
     return message
+
 
 def fixed_xor(buf1, buf2):
     assert len(buf1) == len(buf2)
@@ -78,6 +86,7 @@ def other_char_score(char):
     freq = matrix.get(char, 0)
     return freq
 
+
 def first_char_score(char):
     char = char.lower()
     # http://en.wikipedia.org/wiki/Letter_frequency#Relative_frequencies_of_the_first_letters_of_a_word_in_the_English_language
@@ -112,7 +121,9 @@ def first_char_score(char):
     freq = matrix.get(char, 0)
     return freq
 
+
 def english_score(str):
+    #print str
     # default score is 0
     score = 0
 
@@ -138,35 +149,53 @@ def english_score(str):
                     continue
 
                 score -= 10
+            elif c not in string.printable:
+                score -= 50
             else:
                 score += other_char_score(c)
 
+    if score < 0:
+        score = 0
+
     return score
 
-def brute_force_xor(hexStr):
-    _l = len(hex2bin(hexStr))
+
+def brute_force_xor(bytes):
+    _l = len(bytes)
     scores = {}
 
+    print "looking at", repr(bytes)
     for c in string.printable:
-        result = fixed_xor(hex2bin(hexStr), hex2bin(binascii.b2a_hex(c) * _l))
+        result = fixed_xor(bytes, hex2bin(binascii.b2a_hex(c) * _l))
         scores[c] = english_score(result)
 
-    maxScore = 0
+    maxChar = None
+    maxScore = None
     for char, score in scores.iteritems():
-        if score > maxScore:
+        if maxScore is None or score > maxScore:
             maxScore = score
             maxChar = char
 
-    return {'char': maxChar, 'score': maxScore, 'result': fixed_xor(hex2bin(hexStr), hex2bin(binascii.b2a_hex(maxChar) * _l))}
+    return {'char': maxChar, 'score': maxScore, 'result': fixed_xor(bytes, hex2bin(binascii.b2a_hex(maxChar) * _l))}
 
-#assert brute_force_xor("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736") == {'char': 'X', 'score': 145.468, 'result': bytearray(b"Cooking MC\'s like a pound of bacon")}
+assert brute_force_xor(hex2bin("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")) == {'char': 'X', 'score': 145.468, 'result': bytearray(b"Cooking MC\'s like a pound of bacon")}
+
 
 def challenge_4():
-    scores = []
+    lines = []
     with open('4.txt', 'r') as f:
         for line in f:
             line = line.strip()
-            scores.append(brute_force_xor(line))
+            lines.append(hex2bin(line))
+
+    return brute_force_xor_chunks(lines)
+
+
+def brute_force_xor_chunks(lines):
+    scores = []
+    for line in lines:
+        print line
+        scores.append(brute_force_xor(line))
 
     maxScore = 0
     candidate = None
@@ -177,7 +206,8 @@ def challenge_4():
 
     return candidate
 
-#assert challenge_4() == {'char': '5', 'score': 151.449, 'result': bytearray(b'Now that the party is jumping\n')}
+if ASSERT:
+    assert challenge_4() == {'char': '5', 'score': 151.449, 'result': bytearray(b'Now that the party is jumping\n')}
 
 assert repeating_xor(hex2bin(binascii.hexlify("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal")), hex2bin(binascii.hexlify("ICE"))) == hex2bin("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f")
 
@@ -190,16 +220,96 @@ def edit_dist(str1, str2):
     return _sum
 
 
+def chunkify(l, n):
+    """ Yield successive n-sized chunks from l.
+    """
+    for i in xrange(0, len(l), n):
+        yield l[i:i + n]
+
+
+def transpose(chunks, sz):
+    transposition = []
+    for i in range(1, sz):
+        trans_block = bytearray()
+        for block in chunks:
+            if len(block) != sz - 1:
+                continue
+
+            trans_block.append(block[i - 1])
+
+        transposition.append(trans_block)
+
+    return transposition
+
+
+assert transpose([bytearray("assp"), bytearray("burg"), bytearray("gers")], 5)\
+    == [bytearray("abg"), bytearray("sue"), bytearray("srr"), bytearray("pgs")]
+
+
 def challenge_6():
     b64 = ''
     with open('6.txt', 'r') as f:
         for line in f:
             line = line.strip()
             b64 += line
-    #print base64.b64decode(b64)
-    #print b64
+
+    bytes = base64.b64decode(b64)
+
+    smallestDist = None
+    favKeysize = None
+
+    # XXX try few smallest potential keysizes
+    for samples in range(1, 8):
+        for keysize in range(2, 41):
+            str1 = bytes[:keysize * samples]
+            str2 = bytes[keysize * samples:keysize * 2 * samples]
+            dist = edit_dist(str1, str2)
+            norm = dist / (keysize * samples * 8)
+            if not smallestDist or norm < smallestDist:
+                smallestDist = norm
+                favKeysize = keysize
+
+    chunks = []
+    for chunk in chunkify(bytes, favKeysize):
+        chunks.append(chunk)
+
+    # now we transpose the chunks -- 
+    # for each block, take the first byte, append to transposition array,
+    # then second byte from each block...
+    key = []
+    transposition = transpose(chunks, favKeysize + 1)
+
+    print len(transposition)
+    print transposition
+    # now we have each position of the candidate key to iterate through --
+    # 
+    for block in transposition:
+        for c in string.printable:
+            result = repeating_xor(block, hex2bin(binascii.b2a_hex(c) * favKeysize))
+            print english_score(result)
+
+        key.append(brute_force_xor(block))
+
+    print key
+
+    for c in key:
+        print c['char']
+
+    return bytearray(''.join([c['char'] for c in key]))
 
 
-challenge_6()
+key = challenge_6()
 assert edit_dist("this is a test", "wokka wokka!!!") == 37
+b64 = ''
+bytes = ''
+with open('6.txt', 'r') as f:
+    for line in f:
+        line = line.strip()
+        b64 += line
+
+bytes = bytearray(base64.b64decode(b64))
+
+print repr(key)
+print repr(bytes)
+print repeating_xor(bytes, key)
 
